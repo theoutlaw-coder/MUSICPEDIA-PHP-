86490d6fc803f77afc1f8b0f3f462e23bfb1706d
theoutlaw-coder <lukas.gavran17@gmail.com>
2026-06-12 12:27:58 +0200

    Update brisanje.php

--- PHP/brisanje.php
+++ PHP/brisanje.php
@@ -2,7 +2,6 @@
 session_start();
 include "spoj.php";
 
-// Provjera je li korisnik ulogiran (preporučeno)
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     header("Location: login.php");
     exit;
@@ -11,11 +10,9 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 if (isset($_GET['id'])) {
     $id = (int)$_GET['id'];
 
-    // SQL upit za brisanje
     $sql_brisi = "DELETE FROM koncerti WHERE id = $id";
 
     if (mysqli_query($spoj, $sql_brisi)) {
-        // Uspješno obrisano, vrati na ispis
         header("Location: ispis.php");
         exit();
     } else {
