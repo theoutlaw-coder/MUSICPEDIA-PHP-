9a100c9ef00c756372c05838b46d664fe19045d8
theoutlaw-coder <lukas.gavran17@gmail.com>
2026-06-12 13:21:04 +0200

    Update navbar.php

--- PHP/navbar.php
+++ PHP/navbar.php
@@ -1,13 +1,22 @@
 <nav class="navbar">
-    <a href="pocetna.php">POCETNA</a>
+    <a href="../pocetnastranica.php">POCETNA</a>
+    
     <a href="ispis.php">Popis koncerata</a>
     <a href="narudzbe.php">Narudžbe</a>
-    <a href="kosarica.php">Košarica</a>
-    
-    <?php if(isset($_SESSION['uloga']) && $_SESSION['uloga'] == 'administrator'): ?>
+
+    <?php 
+   
+    ?>
+    <?php if (!isset($_SESSION['uloga']) || $_SESSION['uloga'] !== 'administrator'): ?>
+        <a href="kosarica.php">Košarica</a>
+    <?php endif; ?>
+
+    <?php 
+    ?>
+    <?php if (isset($_SESSION['uloga']) && $_SESSION['uloga'] === 'administrator'): ?>
         <a href="unos.php">Dodaj koncert</a>
         <a href="racuni.php">Računi</a>
     <?php endif; ?>
-    
+
     <a href="logout.php">Logout</a>
 </nav>
