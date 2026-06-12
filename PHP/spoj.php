246959d140668c8e70b693ba3047e3ceb86ce9d6
theoutlaw-coder <lukas.gavran17@gmail.com>
2026-06-12 13:22:48 +0200

    Update spoj.php

--- PHP/spoj.php
+++ PHP/spoj.php
@@ -1,18 +1,14 @@
 <?php
-// Funkcija za spajanje na MySQL server
+
 $spoj = mysqli_connect("localhost", "root", "");
 
-// Provjera je li uspje�no ostvaren spoj na MySQL server
 if (!$spoj) {
     die("<b>Do�lo je do pogre�ke i nismo se mogli spojiti na MySQL server</b>");
 }
 
-// Funkcija za odabir baze podataka na serveru
 if (!mysqli_select_db($spoj, "musicpedia_db")) {
-    // Provjera je li uspje�no odabrana baza podataka
     die("<b>Odabrana je pogre�na baza podataka.</b>");
 }
 
-// Ako smo do�li do ove tocke, spojili smo se na MySQL server i odabrali ispravnu bazu podataka.
-// Mo�ete nastaviti s izvodenjem upita na bazi podataka.
+
 ?>
